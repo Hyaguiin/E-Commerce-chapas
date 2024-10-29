ec24fab3a43a3f9dc94f71396a06dcae8455bcf9
Hyaguiin <137925587+Hyaguiin@users.noreply.github.com>
2024-10-29 17:10:51 -0300

    feature: começo login

--- frontend/ecommerceChapas/src/components/home/home.jsx
+++ frontend/ecommerceChapas/src/components/home/home.jsx
@@ -1,11 +1,20 @@
 // src/components/home/home.jsx
 import React from 'react';
+import Header from '../../components/header/header.jsx';
+import { Dialog, Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/react';
+
+
 
 const Home = () => {
     return (
+        <>
+    
+        <Header />
         <div>
             <h1>Bem-vindo à Home!</h1>
         </div>
+        
+        </>
     );
 };
 
