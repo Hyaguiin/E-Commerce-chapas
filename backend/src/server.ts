44f4a504019c0a01dff925285434025ca10718af
Luan <luanbfarias@outlook.com>
2024-11-05 19:20:54 -0300

    fix: adicionado cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,12 @@ import express from "express";
 import userRouter from "./routers/user-router";
 import "./database/connection";
 import productRouter from "./routers/product-routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
